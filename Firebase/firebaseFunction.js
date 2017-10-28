866eeb0ba8f78c18afc771e4f43e79652e9febdc
Kawaoat <kawaoat@gmail.com>
2017-10-28 17:00:23 +0700

    setup node.js for run script, organize folder stucture

--- Firebase/firebaseFunction.js
+++ Firebase/firebaseFunction.js
@@ -1,54 +1,54 @@
 // onload is similar to constructor
 
-window.onload=function(){
+window.onload = function () {
     showData();
 }
 
 //show data of User's child
-function showData(){
+function showData() {
     //sort by address of child or you can use .orderByKey()
     //to sort by Key
     var firebaseRef = firebase.database().ref("User").orderByChild("address");
-    firebaseRef.once('value').then(function(dataSnapshot){
+    firebaseRef.once('value').then(function (dataSnapshot) {
         //split key and value
-        dataSnapshot.forEach(function(childSnapshot){
+        dataSnapshot.forEach(function (childSnapshot) {
             var childKey = childSnapshot.key;
             var childData = childSnapshot.val();
             console.log(childData)
         })
-    }); 
+    });
 }
 
 //add new child namely "Admin" with value "Root"
-function createChild(){
+function createChild() {
     var firebaseRef = firebase.database().ref();
     firebaseRef.child("Admin").set("Root");
     afterCreateChild();
 }
 
 //add subchild of name namely fname and lname
-function afterCreateChild(){
+function afterCreateChild() {
     var firebaseRef = firebase.database().ref("Admin");
     firebaseRef.child("name/fname").set("Pongsakorn");
     firebaseRef.child("name/lname").set("Jirachan");
 }
 
 //get value from textbox
-function saveData(){
+function saveData() {
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var address = document.getElementById("address");
-    insertData(email.value,password.value,address.value);
+    insertData(email.value, password.value, address.value);
 }
 
 //insert data to database
-function insertData(email,password,address){
+function insertData(email, password, address) {
     var firebaseRef = firebase.database().ref("User");
     //push in JSON format
     firebaseRef.push({
-        email:email,
-        password:password,
-        address:address
+        email: email,
+        password: password,
+        address: address
     });
     //show log of success in browser's inspection
     console.log("insert success")
@@ -56,22 +56,22 @@ function insertData(email,password,address){
 }
 
 //delete User2 from database
-function deleteData(){
+function deleteData() {
     var firebaseRef = firebase.database().ref("User/User2");
     firebaseRef.remove()
-    .then(function(){
-        console.log("remove success");
-    })
-    .catch(function(error){
-        console.log("remove failed: "+ error.message);
-    })
+        .then(function () {
+            console.log("remove success");
+        })
+        .catch(function (error) {
+            console.log("remove failed: " + error.message);
+        })
 }
 
 //updata data of user4
-function updateData(){
+function updateData() {
     var firebaseRef = firebase.database().ref("User/User4");
     //if there are exist "Key", it will be updated,
     //otherwise it will be added
-    firebaseRef.update({address:"Tokyo", Email:"P.Sokkendai@gmail.com"});
+    firebaseRef.update({ address: "Tokyo", Email: "P.Sokkendai@gmail.com" });
     console.log("success");
 }
--- Firebase/login.js
+++ Firebase/login.js
@@ -1,13 +1,13 @@
-window.onload=function(){
+window.onload = function () {
     //to see who is login now
     initApp();
 }
 
-function initApp(){
+function initApp() {
     //check that account is exist
-    firebase.auth().onAuthStateChanged(function(user) {
+    firebase.auth().onAuthStateChanged(function (user) {
         document.getElementById("quickstart-verify-email").disabled = true;
-        if (user){
+        if (user) {
             //login complete and still in web
             var displayName = user.displayName;
             var email = user.email;
@@ -15,43 +15,43 @@ function initApp(){
             var photoURL = user.photoURL;
             var isAnnoymous = user.isAnnoymous;
             var uid = user.uid;
-            var providerData =user.providerData;
-            document.getElementById("quickstart-sign-in-status").textContent="Signed in";
-            document.getElementById("quickstart-sign-in").textContent= "Sign out";
-            document.getElementById("quickstart-account-details").textContent=JSON.stringify(user,null," ");
-            if (!emailVerified){
-                document.getElementById("quickstart-verify-email").disabled=false;
+            var providerData = user.providerData;
+            document.getElementById("quickstart-sign-in-status").textContent = "Signed in";
+            document.getElementById("quickstart-sign-in").textContent = "Sign out";
+            document.getElementById("quickstart-account-details").textContent = JSON.stringify(user, null, " ");
+            if (!emailVerified) {
+                document.getElementById("quickstart-verify-email").disabled = false;
             }
-        }else{
-                document.getElementById("quickstart-sign-in-status").textContent="Signed out";
-                document.getElementById("quickstart-sign-in").textContent="Sign in";
-                document.getElementById("quickstart-account-details").textContent="null";
-            }
-            document.getElementById("quickstart-sign-in").disabled=false;
-        });
-    }
-function signUp(){
+        } else {
+            document.getElementById("quickstart-sign-in-status").textContent = "Signed out";
+            document.getElementById("quickstart-sign-in").textContent = "Sign in";
+            document.getElementById("quickstart-account-details").textContent = "null";
+        }
+        document.getElementById("quickstart-sign-in").disabled = false;
+    });
+}
+function signUp() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    
-    if (reg.test(email) == false){
+
+    if (reg.test(email) == false) {
         alert('Invalid Email Address');
         return;
     }
-    if (password.length<7){
+    if (password.length < 7) {
         alert("Invalid password");
         return;
     }
     //add user account to firebase authentication
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error){
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-        if (errorCode=="auth/weak-password"){
+        if (errorCode == "auth/weak-password") {
             alert("Weak password");
         }
-        else{
+        else {
             alert(errorMessage);
         }
         console.log(error);
@@ -59,58 +59,58 @@ function signUp(){
     initApp();
 }
 
-function toggleSignIn(){
-    if (firebase.auth().currentUser){
+function toggleSignIn() {
+    if (firebase.auth().currentUser) {
         //already login and signout
         firebase.auth().signOut();
     }
-    else{
+    else {
         //not a first time to login, perform login
         var email = document.getElementById("email").value;
         var password = document.getElementById("password").value;
         var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-        
-        if (reg.test(email) == false){
+
+        if (reg.test(email) == false) {
             alert('Invalid Email Address');
             return;
         }
-        if (password.length<7){
+        if (password.length < 7) {
             alert("Invalid password");
             return;
         }
-        firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error){
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
             var errorCode = error.code;
             var errorMessage = error.message;
-            if (errorCode=="auth/wrong-password"){
+            if (errorCode == "auth/wrong-password") {
                 alert("wrong password");
             }
-            else{
+            else {
                 alert(errorMessage);
             }
             console.log(error);
             document.getElementById("quickstart-sign-in").disabled = false;
         });
     }
-    document.getElementById("quickstart-sign-in").disabled=true;
+    document.getElementById("quickstart-sign-in").disabled = true;
 }
 
-function verifyEmail(){
-    firebase.auth().currentUser.sendEmailVerification().then(function(){
+function verifyEmail() {
+    firebase.auth().currentUser.sendEmailVerification().then(function () {
         alert("Email Sent!");
     });
 }
 
-function passwordReset(){
+function passwordReset() {
     var email = document.getElementById("email").value;
-    firebase.auth().sendPasswordResetEmail(email).then(function() {
+    firebase.auth().sendPasswordResetEmail(email).then(function () {
         alert("Re-Password sent!");
-    }).catch(function (error){
+    }).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-        if (errorCode=="auth/user-not-found"){
+        if (errorCode == "auth/user-not-found") {
             alert("User not found");
         }
-        else{
+        else {
             alert(errorMessage);
         }
         console.log(error);
@@ -118,26 +118,26 @@ function passwordReset(){
 }
 
 //facebook login
-function loginFacebook(){
+function loginFacebook() {
     var provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(provider)
-  .then(function(result){
-    var token = result.credential.accessToken;
-    var user = result.user;
-    console.log(token);
-    console.log(user);
-  }).catch(function(error){
-    console.log(error.code);
-    console.log(error.message);
-  })
+    firebase.auth().signInWithPopup(provider)
+        .then(function (result) {
+            var token = result.credential.accessToken;
+            var user = result.user;
+            console.log(token);
+            console.log(user);
+        }).catch(function (error) {
+            console.log(error.code);
+            console.log(error.message);
+        })
 }
 
 //facebook lockout
-function logoutFacebook(){
+function logoutFacebook() {
     firebase.auth().signOut()
-    .then(function() {
-        console.log("signout success");
-    }, function(error) {
-        console.log("signout failed");
-    });
+        .then(function () {
+            console.log("signout success");
+        }, function (error) {
+            console.log("signout failed");
+        });
 }
